2480cb04f9bc7edc110db24e756b947d62f0c7ac
Graimi <113901525+Graimi@users.noreply.github.com>
2023-10-15 15:49:04 +0200

    ClassesData added

---
+++ src/Data/ClassesData.js
@@ -0,0 +1,38 @@
+export const ClassesData = [
+  {
+    Class: 'Boxeo Educativo Infantil',
+    Description: 'Para niños y niñas de 8 a 14 años, sujeta al calendario escolar',
+    Contact: 'SIN CONTACTO'
+  },
+  {
+    Class: 'Only woman',
+    Description: 'Clase exclusivamente destinada a mujeres',
+    Contact: 'SIN CONTACTO O CONTROLADO'
+  },
+  {
+    Class: '12 de Saco',
+    Description:
+      'Sesión de 12 asaltos de golpeo al saco guiados para mejorar la técnica de los golpes y la fuerza',
+    Contact: 'SIN CONTACTO'
+  },
+  {
+    Class: 'Functional Boxing',
+    Description:
+      'Circuitos funcionales específicos para el acondicionamiento físico del Boxeo. Entrenamiento con circuitos funcionales diseñados específicamente para mejorar el acondicionamiento físico en el contexto del boxeo',
+    Contact: 'SIN CONTACTO'
+  },
+  {
+    Class: 'Boxeo Recreativo',
+    Description:
+      'Actividad deportiva dirigida a adolescentes y adultos, con un enfoque en el control del contacto',
+    Contact: 'CONTACTO CONTROLADO'
+  },
+  {
+    Class: 'Boxeo Avanzado y Competición',
+    Description:
+      'Clases dirigidas a adolescentes y adultos con experiencia en boxeo, centradas en el contacto deportivo',
+    Contact: 'CONTACTO DEPORTIVO'
+  }
+];
+
+export default ClassesData;
